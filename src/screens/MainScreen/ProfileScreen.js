c0307116434f5264d079d23446068185ce8f7dc1
Thanh Chu <lilyhanky57@gmail.com>
2022-05-24 00:53:07 +0700

    Log out Version

--- src/screens/MainScreen/ProfileScreen.js
+++ src/screens/MainScreen/ProfileScreen.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import { globalStyles } from '../../../styles/global'
+import { useNavigation } from '@react-navigation/native'
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+  const handleLogOut = () => {
+    navigation.navigate('SignIn');
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.paragraph}>ProfileScreen</Text>
+      <Button title='Log out' onPress={handleLogOut}/>
     </View>
   )
 }
