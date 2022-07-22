e713c5a941b38bf901509a376d9d655f061a2738
Thanh Chu <lilyhanky57@gmail.com>
2022-07-23 01:16:26 +0700

    reply stories

---
+++ src/components/Chats/ReplyBar.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import {TextInput, StyleSheet, View, ActivityIndicator, Keyboard} from 'react-native';
+import {Icon} from 'react-native-elements';
+import {io} from 'socket.io-client';
+import globalColors from '../../../styles/colors';
+import client from '../../../api/client';
+
+const ReplyBar = ({token, userId, navigation}) => {
+  const [sendedMessage, onChangeSendedMessage] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const initiateConvo = () => {
+    client
+      .post(
+        '/chat/initiateConvo',
+        {anotherUserId: userId},
+        {headers: {Authorization: `Bearer ${token}`}},
+      )
+      .then(res => findConvo())
+      .catch(err => findConvo());
+  }
+
+  const findConvo = () => {
+    client
+      .get('/chat/getAllConvos', {headers: {Authorization: `Bearer ${token}`}})
+      .then(response => {
+        const a = response.data
+        for (let i in response.data) {
+          if (a[i].friend[0]._id === userId) {
+            joinRoom(a[i]);
+            break;
+          }
+        }
+      })
+      .catch(error => setLoading(false));
+  }
+  
+  const joinRoom = (item) => {
+    const socket = io.connect('https://orbital-funfit.herokuapp.com/chatFunfit');
+    socket.emit('join', {chatId: item.convoId});
+    sendAMess(socket);
+    setLoading(false);
+  }
+
+  const sendAMess = (socket) => {
+    socket.emit('send new message', {
+      content: sendedMessage,
+      userId: userId,
+    });
+  }
+
+  const onSendRequest = async () => {
+    setLoading(true);
+    await initiateConvo();
+  }
+
+  return (
+    <View style={styles.textInput}>
+      <TextInput
+        placeholder='Send a message'
+        onChangeText={text => onChangeSendedMessage(text)}
+        style={{flex: 1}}
+        clearButtonMode='always'
+      />
+      {loading ? (
+        <ActivityIndicator size='small' />
+      ) : (
+        <Icon 
+          name='send'
+          type='feather'
+          onPress={onSendRequest}
+        />
+      )}
+    </View>
+  )
+}
+
+export default ReplyBar;
+
+const styles = StyleSheet.create({
+  textInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+    borderRadius: 30,
+    position: 'absolute',
+    bottom: 10,
+    margin: 20,
+    backgroundColor: globalColors.cream,
+  },
+})
